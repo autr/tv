0cfa57cca4bf1d4fab7881daf038f00c6aa38331
GSinnott <g@sinnott.cc>
2021-05-25 23:09:59 +0200

    glitch.me ssl

--- src/lib/ListSignup.svelte
+++ src/lib/ListSignup.svelte
@@ -1,8 +1,12 @@
 <script>
+
+	import { browser, dev } from '$app/env'
 	import { ListSignup } from '$lib/ezelte/lib'
+
+	const DEBUG = false
 	
 	const config = {
-		root: 'https://api.autr.tv',
+		root: dev && DEBUG ? 'http://localhost:8000' : 'https://ezipe.glitch.me',
 		user: 'autr',
 		interests: 'artworks:news,code:open source,personal:miscelleny',
 		init: 'vars=artworks,code,personal'
@@ -11,3 +15,5 @@
 </script>
 
 <ListSignup {...config} class="flex column cmb1 sm-max-100 minw18em" />
+
+<!-- <iframe src="https://api.autr.tv" class="w100vw h30em"></iframe> -->
